3e45b76c43e1ae800e2f170bfc188ed7453be91d
Aaliyah <59988961+aaliyah254@users.noreply.github.com>
2025-12-03 23:03:22 -0500

    updated

--- server.js
+++ server.js
@@ -9,22 +9,24 @@ const passportJWT = require("passport-jwt");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
+// Middleware 
 app.use(express.json());
 app.use(cors());
 
-let ExtractJwt = passportJWT.ExtractJwt;
-let JwtStrategy = passportJWT.Strategy;
+//JWT / Passport Setup 
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
 
-let jwtOptions = {
+const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("JWT"),
-  secretOrKey: process.env.JWT_SECRET,
+  secretOrKey: process.env.JWT_SECRET, 
 };
 
-let strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
   if (jwt_payload) {
     next(null, {
       _id: jwt_payload._id,
-      userName: jwt_payload.userName
+      userName: jwt_payload.userName,
     });
   } else {
     next(null, false);
@@ -36,60 +38,82 @@ app.use(passport.initialize());
 
 const auth = passport.authenticate("jwt", { session: false });
 
+
+// Register a new user
 app.post("/api/user/register", (req, res) => {
-  userService.registerUser(req.body)
-    .then(msg => res.json({ message: msg }))
-    .catch(msg => res.status(422).json({ message: msg }));
+  userService
+    .registerUser(req.body)
+    .then((msg) => res.json({ message: msg }))
+    .catch((msg) => res.status(422).json({ message: msg }));
 });
 
+// Login 
 app.post("/api/user/login", (req, res) => {
-  userService.checkUser(req.body)
-    .then(user => {
-
-      // Create JWT payload
-      let payload = {
-        _id: user._id,
-        userName: user.userName
-      };
-      // Sign token
-      let token = jwt.sign(payload, jwtOptions.secretOrKey, {
-        expiresIn: "1h"
-      });
-
-      res.json({ message: "login successful", token: token });
-
+  userService
+    .checkUser(req.body)
+    .then((user) => {
+      const payload = { _id: user._id, userName: user.userName };
+      const token = jwt.sign(payload, jwtOptions.secretOrKey, { expiresIn: "1h" });
+      res.json({ message: "login successful", token });
     })
-    .catch(msg => {
-      res.status(422).json({ message: msg });
-    });
+    .catch((msg) => res.status(422).json({ message: msg }));
 });
 
+// Get favourites
 app.get("/api/user/favourites", auth, (req, res) => {
-  userService.getFavourites(req.user._id)
-    .then(data => res.json(data))
-    .catch(msg => res.status(422).json({ error: msg }));
+  userService
+    .getFavourites(req.user._id)
+    .then((data) => res.json(data))
+    .catch((msg) => res.status(422).json({ error: msg }));
 });
 
+// Add favourite
 app.put("/api/user/favourites/:id", auth, (req, res) => {
-  userService.addFavourite(req.user._id, req.params.id)
-    .then(data => res.json(data))
-    .catch(msg => res.status(422).json({ error: msg }));
+  userService
+    .addFavourite(req.user._id, req.params.id)
+    .then((data) => res.json(data))
+    .catch((msg) => res.status(422).json({ error: msg }));
 });
 
+// Remove Favourite
 app.delete("/api/user/favourites/:id", auth, (req, res) => {
-  userService.removeFavourite(req.user._id, req.params.id)
-    .then(data => res.json(data))
-    .catch(msg => res.status(422).json({ error: msg }));
+  userService
+    .removeFavourite(req.user._id, req.params.id)
+    .then((data) => res.json(data))
+    .catch((msg) => res.status(422).json({ error: msg }));
 });
 
-// Start server after DB connection
-userService.connect()
-  .then(() => {
-    app.listen(HTTP_PORT, () =>
-      console.log("API listening on: " + HTTP_PORT)
-    );
-  })
-  .catch(err => {
-    console.log("unable to start the server: " + err);
-    process.exit();
-  });
+
+let connected = false;
+async function ensureConnect() {
+  if (!connected) {
+    await userService.connect();
+    connected = true;
+    console.log("DB connected");
+  }
+}
+
+if (process.env.VERCEL) {
+  module.exports = async (req, res) => {
+    try {
+      await ensureConnect();
+      return app(req, res);
+    } catch (err) {
+      console.error("Handler error:", err);
+      res.statusCode = 500;
+      res.end("Internal Server Error");
+    }
+  };
+} else {
+
+  ensureConnect()
+    .then(() => {
+      app.listen(HTTP_PORT, () => {
+        console.log("API listening on: " + HTTP_PORT);
+      });
+    })
+    .catch((err) => {
+      console.log("unable to start the server: " + err);
+      process.exit(1);
+    });
+}
